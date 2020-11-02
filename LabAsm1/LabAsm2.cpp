4e43bf4c125619bea12e903e0cea4fc97e0fed24
DerFreimensch <k.d.markov@outlook.com>
2020-11-02 15:30:56 +0300

    add 3 more tasks

---
+++ LabAsm1/LabAsm2.cpp
@@ -0,0 +1,39 @@
+﻿
+
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long int a, b;
+    cout << "Print A\n";
+    cin >> a;
+    cout << "Print B\n";
+    cin >> b;
+    //-2^31<=(2A^2+11A)/b<=2^31-1
+    // -2^31<=b<=2^31-1 b!=0
+    // (-2^31)/11<=b<=(2^31-1)/11
+    __asm {
+        mov eax, a
+        mov ebx, 11
+        mul ebx
+        mov ecx, eax
+        mov eax,2
+<<<<<<< HEAD
+        mul a
+        mul a
+        add eax,ecx
+        div b
+=======
+        imul a
+        imul a
+        add eax,ecx
+        cdq
+        idiv b
+>>>>>>> 912a57ff07dd318351f538be5aeff2d5d9e2438f
+        mov a, eax
+        mov b, edx
+    }
+    cout << "Celoe: " << a << " " << "Ostatok: " << b << endl;
+}*/
